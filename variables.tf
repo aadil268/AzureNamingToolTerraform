feadedecd418fa84bcd2bea5c06c82ce6cb1e885
Aadil Abbasi <aadil.abbasi@cchellenic.com>
2025-08-03 12:56:15 -0400

    Update variables.tf to include new environment variable

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "The Azure region where resources will be created"
   type        = string
-  default     = "East US"
+  default     = "North Europe"
 }
 
 variable "app_service_plan_name" {
